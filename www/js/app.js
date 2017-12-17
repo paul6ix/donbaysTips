49627d244f76d93984850a8b1489505fc8c49d75
paul6ix <okporp@gmail.com>
2017-12-17 16:46:23 +0100

    1.Changed sidemenu to tab menu 2. added pull request from website into the app successfull 3. added tabs for news and livescore

--- www/js/app.js
+++ www/js/app.js
@@ -5,20 +5,74 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
-.run(function($ionicPlatform) {
-  $ionicPlatform.ready(function() {
-    if(window.cordova && window.cordova.plugins.Keyboard) {
-      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
-      // for form inputs)
-      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+  .run(function ($ionicPlatform) {
+    $ionicPlatform.ready(function () {
+      if (window.cordova && window.cordova.plugins.Keyboard) {
+        // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+        // for form inputs)
+        cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
 
-      // Don't remove this line unless you know what you are doing. It stops the viewport
-      // from snapping when text inputs are focused. Ionic handles this internally for
-      // a much nicer keyboard experience.
-      cordova.plugins.Keyboard.disableScroll(true);
-    }
-    if(window.StatusBar) {
-      StatusBar.styleDefault();
-    }
+        // Don't remove this line unless you know what you are doing. It stops the viewport
+        // from snapping when text inputs are focused. Ionic handles this internally for
+        // a much nicer keyboard experience.
+        cordova.plugins.Keyboard.disableScroll(true);
+      }
+      if (window.StatusBar) {
+        StatusBar.styleDefault();
+      }
+    });
+  })
+  .config(function ($ionicConfigProvider) {
+    $ionicConfigProvider.tabs.position('bottom');
+  })
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state('main', {
+        url: '/main',
+        abstract: true,
+        templateUrl: 'templates/menu.html',
+        controller: 'MenuController'
+      })
+      /*Experimental features for tabs menu */
+      .state('main.tips', {
+        url: '/tips',
+        views: {
+          'tab-tips': {
+            templateUrl: 'templates/tab-tips.html',
+            controller: 'MenuController'
+
+          }
+        }
+
+      })
+      .state('main.news', {
+        url: '/news',
+        views: {
+          'tab-news': {
+            templateUrl: 'templates/tab-news.html',
+            controller: 'NewsController'
+          }
+        }
+      })
+      .state('main.livescore', {
+        url: '/livescore',
+        views: {
+          'tab-livescore': {
+            templateUrl: 'templates/tab-livescore.html',
+            controller: 'LivescoreController'
+          }
+        }
+      })
+      .state('main.MenuContents', {
+        url: '/MenuContents',
+        templateUrl: 'templates/MenuContents.html',
+        controller: 'MenuController'
+      })
+      .state('main.PostDetails', {
+        url: '/PostDetail/:PostId',
+        templateUrl: 'templates/PostDetails.html',
+        controller: 'PostController'
+      });
+    $urlRouterProvider.otherwise('/main/MenuContents');
+
   });
-})
--- www/js/controller.js
+++ www/js/controller.js
@@ -2,7 +2,20 @@
  * Created by Paul Chidi on 12/13/2017.
  */
 angular.module('starter')
-  .controller('TempController', function () {
-    alert('hey 6ix whatsup')
+  .controller('MenuController', function ($scope, $http) {
+    $scope.categories = [];
+    $http.get("http://donbaystips.com.ng/api/get_category_index/").then(function (returnedData) {
+        $scope.categories = returnedData.data.categories;
+        console.log(returnedData);
+      },
+      function (error) {
+        console.log(error);
+      })
 
   })
+  .controller('PostController', function () {
+  })
+  .controller('NewsController', function () {
+  })
+  .controller('LivescoreController', function () {
+  });
