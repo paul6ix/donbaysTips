a96168874b40b61510027cf556943cf5296f4cbc
paul6ix <okporp@gmail.com>
2017-12-18 15:57:10 +0100

    1. football news api now working 2. donbaytips api now implemented

--- www/js/controller.js
+++ www/js/controller.js
@@ -2,10 +2,13 @@
  * Created by Paul Chidi on 12/13/2017.
  */
 angular.module('starter')
-  .controller('MenuController', function ($scope, $http) {
-    $scope.categories = [];
-    $http.get("http://donbaystips.com.ng/api/get_category_index/").then(function (returnedData) {
-        $scope.categories = returnedData.data.categories;
+  .controller('MenuController', function ($scope, $http, $sce) {
+    $scope.posts = [];
+    $http.get("http://donbaystips.com.ng/api/get_posts/").then(function (returnedData) {
+        $scope.posts = returnedData.data.posts;
+        $scope.posts.forEach(function (element, index, array) {
+          element.title = $sce.trustAsHtml(element.title);
+        });
         console.log(returnedData);
       },
       function (error) {
@@ -15,7 +18,16 @@ angular.module('starter')
   })
   .controller('PostController', function () {
   })
-  .controller('NewsController', function () {
+  .controller('NewsController', function ($scope, $http, $sce) {
+    $scope.articles = [];
+    $http.get("https://newsapi.org/v2/top-headlines?sources=bbc-sport&apiKey=d3a6e97988bd43a1833061e16e5b9b5c").then(function (newsData) {
+        $scope.articles = newsData.data.articles;
+        console.log(newsData);
+      },
+      function (error) {
+        console.log(error);
+      })
+
   })
   .controller('LivescoreController', function () {
   });
